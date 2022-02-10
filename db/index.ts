21c30a613f34c07af70fa4402c96d010494aea9d
ManasAtabaev <manas.atabaev@gmail.com>
2022-02-10 13:30:12 +0800

    fix imports

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,5 @@
 import * as dbConfig from '../database.json';
-import pgPromise from 'pg-promise';
-import { IMain } from 'pg-promise';
+import pgPromise, { IMain } from 'pg-promise';
 
 const pgp: IMain = pgPromise({
   capSQL: true,
